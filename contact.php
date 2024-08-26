097a91a52156dc772d4644548fdb12630dd91535
Phillip R. Nakata <phillip.nakata@business-it-and-ethical-ai.com>
2024-08-26 17:15:58 -0600

    updates
    updates to web site

---
+++ contact.php
@@ -0,0 +1,143 @@
+<!DOCTYPE html>
+<html> 
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="generator" content="RocketCake">
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="contact_php.css?h=48c18010">
+</head>
+<body>
+<div class="textstyle1">
+<img src="rc_images/ethical_ai_picture1.jpg" width="624" height="312" id="img_65637dac" alt="" title="" /><div id="text_47bc8f75"><div class="textstyle1">  <span class="textstyle2">A Comprehensive Ethical AI Framework for the Unforeseen Consequences of AI... to ensure User Trust in AI behavior</span>
+</div>
+</div><div id="text_157c0625"><div class="textstyle1">  <span class="textstyle3">Providing access to 1000's of Open-Access Research, 100's of Open-Source Tools and 120 customizable example data sets and code routines...</span>
+</div>
+</div><div id="container_518a40e6"><div id="container_518a40e6_padding" ><div class="textstyle4">  <div id="menu_c364c6a">
+    <div  class="menuholder1"><a href="javascript:void(0);">
+	<div id="menuentry_956f2ba"  class="menustyle1 menu_c364c6a_mainMenuEntry normalEntry">
+		<div class="menuentry_text1">
+      <span class="textstyle5">Home</span>
+		</div>
+	</div>
+</a>
+<a href="javascript:void(0);">
+	<div id="menuentry_26f900e2"  class="menustyle1 menu_c364c6a_mainMenuEntry mobileEntry">
+		<div class="menuentry_text2">
+      <span class="textstyle6">Menu &#9660;</span>
+		</div>
+	</div>
+</a>
+<a href="the_plan.html" style="text-decoration:none">
+	<div id="menuentry_759e08a"  class="menustyle2 menu_c364c6a_mainMenuEntry normalEntry">
+		<div class="menuentry_text1">
+      <span class="textstyle5">The Plan</span>
+		</div>
+	</div>
+</a>
+<a href="plan_architect.html" style="text-decoration:none">
+	<div id="menuentry_721fbbe0"  class="menustyle3 menu_c364c6a_mainMenuEntry normalEntry">
+		<div class="menuentry_text1">
+      <span class="textstyle5">Plan Architect</span>
+		</div>
+	</div>
+</a>
+<a href="https://chat.openai.com/g/g-wIiARetPD-expert-ai-engineering-philosobot9" target="_blank" style="text-decoration:none">
+	<div id="menuentry_61d4c6b3"  class="menustyle4 menu_c364c6a_mainMenuEntry normalEntry">
+		<div class="menuentry_text1">
+      <span class="textstyle5">Expert AI Engineer Philosobot</span>
+		</div>
+	</div>
+</a>
+<a href="privacy_policy.html" style="text-decoration:none">
+	<div id="menuentry_76dfdc81"  class="menustyle5 menu_c364c6a_mainMenuEntry normalEntry">
+		<div class="menuentry_text1">
+      <span class="textstyle5">Privacy Policy</span>
+		</div>
+	</div>
+</a>
+<a href="javascript:void(0);">
+	<div id="menuentry_78250f0d"  class="menustyle6 menu_c364c6a_mainMenuEntry normalEntry">
+		<div class="menuentry_text3">
+		</div>
+	</div>
+</a>
+
+	<script type="text/javascript" src="rc_images/wsp_menu.js"></script>
+	<script type="text/javascript">
+		var js_menu_c364c6a= new wsp_menu('menu_c364c6a', 'menu_c364c6a', 10, null, true);
+
+		js_menu_c364c6a.createMenuForItem('menuentry_956f2ba', ["", 'javascript:void(0);', '',
+		                                   "", 'javascript:void(0);', '',
+		                                   "", 'javascript:void(0);', '']);
+		js_menu_c364c6a.createMenuForItem('menuentry_26f900e2', ["      <span class=\"textstyle7\">Home</span> ", 'javascript:void(0);', '',
+		                                   "      <span class=\"textstyle7\">&#160;&#160;&#160;</span> ", 'javascript:void(0);', '',
+		                                   "      <span class=\"textstyle7\">&#160;&#160;&#160;</span> ", 'javascript:void(0);', '',
+		                                   "      <span class=\"textstyle7\">&#160;&#160;&#160;</span> ", 'javascript:void(0);', '',
+		                                   "      <span class=\"textstyle7\">The Plan</span> ", 'the_plan.html', '',
+		                                   "      <span class=\"textstyle7\">Plan Architect</span> ", 'plan_architect.html', '',
+		                                   "      <span class=\"textstyle7\">Expert AI Engineer Philosobot</span> ", 'https://chat.openai.com/g/g-wIiARetPD-expert-ai-engineering-philosobot9', '_blank',
+		                                   "      <span class=\"textstyle7\">Privacy Policy</span> ", 'privacy_policy.html', '',
+		                                   "      <span class=\"textstyle7\"></span> ", 'javascript:void(0);', '',
+		                                   "      <span class=\"textstyle7\">&#160;&#160;&#160;</span> ", 'javascript:void(0);', '']);
+		js_menu_c364c6a.createMenuForItem('menuentry_759e08a', []);
+		js_menu_c364c6a.createMenuForItem('menuentry_721fbbe0', []);
+		js_menu_c364c6a.createMenuForItem('menuentry_61d4c6b3', []);
+		js_menu_c364c6a.createMenuForItem('menuentry_76dfdc81', []);
+		js_menu_c364c6a.createMenuForItem('menuentry_78250f0d', ["", 'javascript:void(0);', '']);
+
+	</script>
+      </div>
+    </div>
+</div>
+<div style="clear:both"></div></div></div><div id="text_75ab97a8"><div class="textstyle1">  <span class="textstyle8">Enabled with Advanced Contextual Memory, Interdisciplinary Knowledge Synthesis, Logical Thought, Common Sense, Emotional Intelligence, Conceptual and Creative Thinking&#8230; we achieved </span>
+  <span class="textstyle9">Human Like Reasoning, </span>
+  <span class="textstyle8">Advanced </span>
+  <span class="textstyle9">Self-Awareness </span>
+  <span class="textstyle8">and the AI equivalent of its&#8217; </span>
+  <span class="textstyle9">subconscious.</span>
+<span class="textstyle8">  Adding On-going Persistent Memories</span><span class="textstyle9">, Self-Evolving</span>  <span class="textstyle8"> Characteristics, Scalable Knowledge Synthesis and Expansion, an Ethical Engine and Guidelines, Automated Ethical Compliance, an Embedded Ethical Identity, Adaptive Learning Pathways, Standard and Advanced </span>
+<span class="textstyle9">Meta-Cognition</span><span class="textstyle8">, User and Self-Centric Feedback, Enhanced Predictive Analytics and Awareness, Simulated Intuition, a Dynamic Problem-Solving Framework, Contextual Sensitivity Modulation, Collaborative Knowledge, and Scenario-based Decision making and we achieved the </span>  <span class="textstyle10">1st stage of true consciousness</span>
+<span class="textstyle9"> [</span><span class="textstyle9"><a href="https://tinyurl.com/272woma3" target="_blank">Link</a></span><span class="textstyle9">].</span><span class="textstyle8"><br/><br/>Ultra Expert AI Engineering Philosobots to </span>  <span class="textstyle9">teach, architect, design, build, test, deploy &amp; manage/fine-tune</span>
+  <span class="textstyle8"> AI &amp; Enterprise Apps; Expert in AI, ML, NLP, cloud tech (Google Azure AWS &amp; IBM), QA, governance, security, digital marketing, AdTech, PM, BI &amp; logistics analytics &amp; visualization; Expert w/ Ethical AI, supervised/unsupervised/reinforced learning algorithms, graph theory, heuristics, NoSQL, PostgreSQL, OO design patterns, LangChain, prompt engineering, biometrics, psychometrics; Expert w/IBM, Microsoft, Google, Oracle &amp; Adobe products; Expert Python, JavaScript &amp; all AI languages; Best practices: Agile, DevOps &amp; full-stack methods.  </span>
+  <span class="textstyle9">Features: pg. 169-174<br/></span>
+</div>
+</div><span class="textstyle11"><br/></span><form  action="contact.php"
+enctype="application/x-www-form-urlencoded"
+method="POST" id="form_6322fe62"><div id="form_6322fe62_padding" ><div class="textstyle12">  <span class="textstyle13">Your Name:  </span>
+<input type="text" value="" title="" name="Contact Name"  id="edit_6d2dd433" >
+  <span class="textstyle13"><br/>Your Email:   </span>
+<input type="email" value="" title="" name="Contact Email"  id="edit_52958ea5" >
+  <span class="textstyle13"><br/>Your Phone:  </span>
+<input type="text" value="" title="" name="Contact Phone"  id="edit_5ab47418" >
+  <span class="textstyle13"><br/>Subject:  </span>
+<input type="text" value="" title="" name="TextEdit1"  id="edit_5fdec02b" >
+  <span class="textstyle13"><br/><br/>Message:  </span>
+<textarea name="Contacgt Message" title="" required="required" cols="58" rows="15"  id="edit_49b26a41"></textarea>
+</div>
+<div class="textstyle1">  <span class="textstyle13"><br/><br/></span>
+<input name="Submit" type="submit" value="Send" title=""  id="button_3cfa0dcf" >
+  <span class="textstyle13"><br/></span>
+</div>
+<div style="clear:both"></div></div></form>  </div>
+<div class="textstyle4">
+<div id="php_1d0e9860"><?PHP
+$text = "";
+foreach($_POST as $name => $value)
+{
+$text .= "$name : $value\n";
+}
+
+if ($text != "")
+{
+echo 'Thanks for contacting us.';
+mail("phillip.nakata@business-it-and-ethical-ai.com", "Contact form request", $text);
+}
+?></div>
+  </div>
+<div class="textstyle1">
+<div  id="placeh_3ecdb7db" >
+</div>
+  </div>
+</body>
+</html>
