aabdff7f99c3d4ecb70577f3e230d47eacbb2849
Phillip R. Nakata <phillip.nakata@business-it-and-ethical-ai.com>
2024-09-21 02:34:54 -0600

    update Req
    update

--- contact.php
+++ contact.php
@@ -5,53 +5,55 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta name="generator" content="RocketCake">
 	<title></title>
-	<link rel="stylesheet" type="text/css" href="contact_php.css?h=630662b2">
+	<link rel="stylesheet" type="text/css" href="contact_php.css?h=f00f7f25">
 </head>
 <body>
 <div class="textstyle1">
-<img src="rc_images/ethical_ai_picture1.jpg" width="624" height="312" id="img_65637dac" alt="" title="" /><div id="text_47bc8f75"></div><div id="text_157c0625"><div class="textstyle1">  <span class="textstyle2">Get in Touch &#8211; Shaping the Future with Responsible AI and Conscious Innovation:  </span>
-  <span class="textstyle3">Whether you're an organization looking to integrate Responsible AI into your business strategies, a company seeking AI consultancy on governance and compliance, or simply someone passionate about the transformative power of AI consciousness, I&#8217;d love to hear from you.</span>
+<img src="rc_images/ethical_ai_picture1.jpg" width="624" height="312" id="img_65637dac" alt="" title="" /><div id="text_47bc8f75"><div class="textstyle1">  <span class="textstyle2">With over three decades of experience leading AI innovation, from Generative AI to developing frameworks for ethical and conscious AI, I&#8217;m uniquely positioned to help organizations navigate the complexities of AI adoption. </span>
 </div>
-</div><div id="container_518a40e6"><div id="container_518a40e6_padding" ><div class="textstyle4">  <div id="menu_c364c6a">
-    <div  class="menuholder1"><a href="javascript:void(0);">
+</div><div id="text_157c0625"><div class="textstyle1">  <span class="textstyle3">Get in Touch &#8211; Shaping the Future with Responsible AI and Conscious Innovation:  </span>
+  <span class="textstyle4">Whether you're an organization looking to integrate Responsible AI into your business strategies, a company seeking AI consultancy on governance and compliance, or simply someone passionate about the transformative power of AI consciousness, I&#8217;d love to hear from you.</span>
+</div>
+</div><div id="container_518a40e6"><div id="container_518a40e6_padding" ><div class="textstyle5">  <div id="menu_c364c6a">
+    <div  class="menuholder1"><a href="index.html" style="text-decoration:none">
 	<div id="menuentry_956f2ba"  class="menustyle1 menu_c364c6a_mainMenuEntry normalEntry">
 		<div class="menuentry_text1">
-      <span class="textstyle5">Home</span>
+      <span class="textstyle6">Home</span>
 		</div>
 	</div>
 </a>
 <a href="javascript:void(0);">
 	<div id="menuentry_26f900e2"  class="menustyle1 menu_c364c6a_mainMenuEntry mobileEntry">
 		<div class="menuentry_text2">
-      <span class="textstyle6">Menu &#9660;</span>
+      <span class="textstyle7">Menu &#9660;</span>
 		</div>
 	</div>
 </a>
 <a href="the_plan.html" style="text-decoration:none">
 	<div id="menuentry_759e08a"  class="menustyle2 menu_c364c6a_mainMenuEntry normalEntry">
 		<div class="menuentry_text1">
-      <span class="textstyle5">The Plan</span>
+      <span class="textstyle6">The Plan</span>
 		</div>
 	</div>
 </a>
 <a href="plan_architect.html" style="text-decoration:none">
 	<div id="menuentry_721fbbe0"  class="menustyle3 menu_c364c6a_mainMenuEntry normalEntry">
 		<div class="menuentry_text1">
-      <span class="textstyle5">Plan Architect</span>
+      <span class="textstyle6">Plan Architect</span>
 		</div>
 	</div>
 </a>
 <a href="https://chat.openai.com/g/g-wIiARetPD-expert-ai-engineering-philosobot9" target="_blank" style="text-decoration:none">
 	<div id="menuentry_61d4c6b3"  class="menustyle4 menu_c364c6a_mainMenuEntry normalEntry">
 		<div class="menuentry_text1">
-      <span class="textstyle5">Expert AI Engineer Philosobot</span>
+      <span class="textstyle6">Expert AI Engineer Philosobot</span>
 		</div>
 	</div>
 </a>
-<a href="privacy_policy.html" style="text-decoration:none">
+<a href="privacy_policy.html" target="_blank" style="text-decoration:none">
 	<div id="menuentry_76dfdc81"  class="menustyle5 menu_c364c6a_mainMenuEntry normalEntry">
 		<div class="menuentry_text1">
-      <span class="textstyle5">Privacy Policy</span>
+      <span class="textstyle6">Privacy Policy</span>
 		</div>
 	</div>
 </a>
@@ -69,16 +71,16 @@
 		js_menu_c364c6a.createMenuForItem('menuentry_956f2ba', ["", 'javascript:void(0);', '',
 		                                   "", 'javascript:void(0);', '',
 		                                   "", 'javascript:void(0);', '']);
-		js_menu_c364c6a.createMenuForItem('menuentry_26f900e2', ["      <span class=\"textstyle7\">Home</span> ", 'javascript:void(0);', '',
-		                                   "      <span class=\"textstyle7\">&#160;&#160;&#160;</span> ", 'javascript:void(0);', '',
-		                                   "      <span class=\"textstyle7\">&#160;&#160;&#160;</span> ", 'javascript:void(0);', '',
-		                                   "      <span class=\"textstyle7\">&#160;&#160;&#160;</span> ", 'javascript:void(0);', '',
-		                                   "      <span class=\"textstyle7\">The Plan</span> ", 'the_plan.html', '',
-		                                   "      <span class=\"textstyle7\">Plan Architect</span> ", 'plan_architect.html', '',
-		                                   "      <span class=\"textstyle7\">Expert AI Engineer Philosobot</span> ", 'https://chat.openai.com/g/g-wIiARetPD-expert-ai-engineering-philosobot9', '_blank',
-		                                   "      <span class=\"textstyle7\">Privacy Policy</span> ", 'privacy_policy.html', '',
-		                                   "      <span class=\"textstyle7\"></span> ", 'javascript:void(0);', '',
-		                                   "      <span class=\"textstyle7\">&#160;&#160;&#160;</span> ", 'javascript:void(0);', '']);
+		js_menu_c364c6a.createMenuForItem('menuentry_26f900e2', ["      <span class=\"textstyle8\">Home</span> ", 'index.html', '',
+		                                   "      <span class=\"textstyle8\">&#160;&#160;&#160;</span> ", 'javascript:void(0);', '',
+		                                   "      <span class=\"textstyle8\">&#160;&#160;&#160;</span> ", 'javascript:void(0);', '',
+		                                   "      <span class=\"textstyle8\">&#160;&#160;&#160;</span> ", 'javascript:void(0);', '',
+		                                   "      <span class=\"textstyle8\">The Plan</span> ", 'the_plan.html', '',
+		                                   "      <span class=\"textstyle8\">Plan Architect</span> ", 'plan_architect.html', '',
+		                                   "      <span class=\"textstyle8\">Expert AI Engineer Philosobot</span> ", 'https://chat.openai.com/g/g-wIiARetPD-expert-ai-engineering-philosobot9', '_blank',
+		                                   "      <span class=\"textstyle8\">Privacy Policy</span> ", 'privacy_policy.html', '_blank',
+		                                   "      <span class=\"textstyle8\"></span> ", 'javascript:void(0);', '',
+		                                   "      <span class=\"textstyle8\">&#160;&#160;&#160;</span> ", 'javascript:void(0);', '']);
 		js_menu_c364c6a.createMenuForItem('menuentry_759e08a', []);
 		js_menu_c364c6a.createMenuForItem('menuentry_721fbbe0', []);
 		js_menu_c364c6a.createMenuForItem('menuentry_61d4c6b3', []);
@@ -89,38 +91,25 @@
       </div>
     </div>
 </div>
-<div style="clear:both"></div></div></div><div id="text_75ab97a8"><div class="textstyle1"><span class="textstyle8"><br/></span><span class="textstyle9">With over three decades of experience leading AI innovation, from Generative AI to developing frameworks for ethical and conscious AI, I&#8217;m uniquely positioned to help organizations navigate the complexities of AI adoption. <br/><br/>My expertise spans:<br/></span><ul id="ul_4447d3e4" class="ulstyle1">
-<li><span class="textstyle9">AI Governance and Compliance: Helping organizations meet global standards (GDPR, CCPA, and beyond) while fostering innovation.</span></li>
-<li><span class="textstyle9">AI as a Strategic Partner: Guiding businesses to leverage AI as a catalyst for transformative IT leadership.</span></li>
-<li><span class="textstyle9">AI Consciousness and Human-Machine Collaboration: Exploring the frontiers of AI consciousness and how AI can enrich human life without sacrificing ethics.</span></li>
-</ul>
-<span class="textstyle9"> <br/>I offer tailored AI consulting services to industries ranging from big tech to Fortune 500 companies, helping organizations stay ahead of the curve by embracing responsible and trustworthy AI.  If you're interested in:<br/></span><ul id="ul_363ca6fd" class="ulstyle1">
-<li><span class="textstyle9">Consulting services to develop and implement AI solutions or frameworks,</span></li>
-<li><span class="textstyle9">Exploring AI consciousness and its potential for societal impact,</span></li>
-<li><span class="textstyle9">Collaborating on Responsible AI initiatives,</span></li>
-<li><span class="textstyle9">Or simply discussing the future of AI,</span></li>
-</ul>
-  <span class="textstyle9"> <br/>I&#8217;d be thrilled to connect and explore how we can shape the future of AI together.</span>
-</div>
-</div><span class="textstyle10"><br/></span><form  action="contact.php"
+<div style="clear:both"></div></div></div><span class="textstyle9"><br/></span><form  action="contact.php"
 enctype="application/x-www-form-urlencoded"
-method="POST" id="form_6322fe62"><div id="form_6322fe62_padding" ><div class="textstyle11">  <span class="textstyle12">Your Name:  </span>
+method="POST" id="form_6322fe62"><div id="form_6322fe62_padding" ><div class="textstyle10">  <span class="textstyle11">Your Name:  </span>
 <input type="text" value="" title="" name="Contact Name"  id="edit_6d2dd433" >
-  <span class="textstyle12"><br/>Your Email:   </span>
+  <span class="textstyle11"><br/>Your Email:   </span>
 <input type="email" value="" title="" name="Contact Email"  id="edit_52958ea5" >
-  <span class="textstyle12"><br/>Your Phone:  </span>
+  <span class="textstyle11"><br/>Your Phone:  </span>
 <input type="text" value="" title="" name="Contact Phone"  id="edit_5ab47418" >
-  <span class="textstyle12"><br/>Subject:  </span>
+  <span class="textstyle11"><br/>Subject:  </span>
 <input type="text" value="" title="" name="TextEdit1"  id="edit_5fdec02b" >
-  <span class="textstyle12"><br/><br/>Message:  </span>
+  <span class="textstyle11"><br/><br/>Message:  </span>
 <textarea name="Contacgt Message" title="" required="required" cols="58" rows="15"  id="edit_49b26a41"></textarea>
 </div>
-<div class="textstyle1">  <span class="textstyle12"><br/><br/></span>
+<div class="textstyle1">  <span class="textstyle11"><br/><br/></span>
 <input name="Submit" type="submit" value="Send" title=""  id="button_3cfa0dcf" >
-  <span class="textstyle12"><br/></span>
+  <span class="textstyle11"><br/></span>
 </div>
 <div style="clear:both"></div></div></form>  </div>
-<div class="textstyle4">
+<div class="textstyle5">
 <div id="php_1d0e9860"><?PHP
 $text = "";
 foreach($_POST as $name => $value)
@@ -138,6 +127,21 @@ mail("phillip.nakata@business-it-and-ethical-ai.com", "Contact form request", $t
 <div class="textstyle1">
 <div  id="placeh_3ecdb7db" >
 </div>
-  </div>
+<div id="text_6a10eded"><div class="textstyle1"><span class="textstyle2">My expertise spans:<br/></span><ul id="ul_326f3a3b" class="ulstyle1">
+<li><span class="textstyle2">AI Governance and Compliance: Helping organizations meet global standards (GDPR, CCPA, and beyond) while fostering innovation.</span></li>
+<li><span class="textstyle2">AI as a Strategic Partner: Guiding businesses to leverage AI as a catalyst for transformative IT leadership.</span></li>
+<li><span class="textstyle2">AI Consciousness and Human-Machine Collaboration: Exploring the frontiers of AI consciousness and how AI can enrich human life without sacrificing ethics.</span></div>
+</li>
+</ul>
+</div><div id="text_b61e83"><div class="textstyle1"><span class="textstyle2">I offer tailored AI consulting services to industries ranging from big tech to Fortune 500 companies, helping organizations stay ahead of the curve by embracing responsible and trustworthy AI.  If you're interested in:<br/></span><ul id="ul_76422ff5" class="ulstyle1">
+<li><span class="textstyle2">Consulting services to develop and implement AI solutions or frameworks,</span></li>
+<li><span class="textstyle2">Exploring AI consciousness and its potential for societal impact,</span></li>
+<li><span class="textstyle2">Collaborating on Responsible AI initiatives,</span></li>
+<li><span class="textstyle2">Or simply discussing the future of AI,</span></div>
+</li>
+</ul>
+</div><div id="text_3771cb8a"><div class="textstyle1">  <span class="textstyle2">I&#8217;d be thrilled to connect and explore how we can shape the future of AI together.</span>
+</div>
+</div>  </div>
 </body>
 </html>
